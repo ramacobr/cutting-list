f6095ca67ee5eaadee097658cd4568ac7244f606
Pedro Edra Sousa <pedroedrasousa@gmail.com>
2017-05-30 10:55:37 +0100

    Fixed geolite2 map resource loading

--- cutlistoptimizerapp/src/main/java/spring/TrackingService.java
+++ cutlistoptimizerapp/src/main/java/spring/TrackingService.java
@@ -1,9 +1,10 @@
 package spring;
 
 import com.maxmind.geoip2.DatabaseReader;
-import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.maxmind.geoip2.model.AsnResponse;
 import com.maxmind.geoip2.model.CityResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -14,19 +15,20 @@ import java.net.InetAddress;
 @Service
 public class TrackingService {
 
+    @Autowired
+    private ApplicationContext context;
+
     private DatabaseReader cityDatabaseReader;
 
     private DatabaseReader asnDatabaseReader;
 
     @PostConstruct
     public void init() {
-        ClassLoader classLoader = getClass().getClassLoader();
-
         try {
-            File cityDatabaseFile = new File(classLoader.getResource("GeoLite2-City_20170502/GeoLite2-City.mmdb").getFile());
+            File cityDatabaseFile = context.getResource("classpath:GeoLite2-City_20170502/GeoLite2-City.mmdb").getFile();
             cityDatabaseReader = new DatabaseReader.Builder(cityDatabaseFile).build();
 
-            File asnDatabaseFile = new File(classLoader.getResource("GeoLite2-ASN_20170516/GeoLite2-ASN.mmdb").getFile());
+            File asnDatabaseFile = context.getResource("classpath:GeoLite2-ASN_20170516/GeoLite2-ASN.mmdb").getFile();
             asnDatabaseReader = new DatabaseReader.Builder(asnDatabaseFile).build();
         } catch (IOException e) {
             e.printStackTrace();
